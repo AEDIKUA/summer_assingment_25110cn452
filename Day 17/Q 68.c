f5e2d3c1eff0afb8a0c92f4cc21019d32110daf3
AEDIKUA <adarshkumar06834@gmail.com>
2026-06-24 18:14:35 +0530

    Create Q 68.c

---
+++ Day 17/Q 68.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {10, 20, 30, 40};
+    int b[] = {30, 40, 50, 60};
+    int i, j;
+
+    printf("Common elements: ");
+
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
+            if(a[i] == b[j]) {
+                printf("%d ", a[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
