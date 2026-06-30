9f909dabb979b51b7c592ceecc9c5c33a64ce18b
AEDIKUA <adarshkumar06834@gmail.com>
2026-06-30 07:02:39 +0530

    Create Q 86.c

---
+++ DAY 22/Q 86.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    if (str[0] == '\n') {
+        words = 0;
+    } else {
+        while (str[i] != '\0') {
+            if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+                words++;
+            i++;
+        }
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
