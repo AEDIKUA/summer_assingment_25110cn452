2587bda44b1ad5edf8a053d327b98c7523f07539
AEDIKUA <adarshkumar06834@gmail.com>
2026-06-30 07:19:33 +0530

    Create Q 107.c

---
+++ DAY 27/Q 107.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float basic, hra, da, salary;
+
+    printf("Enter Basic Salary: ");
+    scanf("%f", &basic);
+
+    hra = basic * 0.20;
+    da = basic * 0.10;
+    salary = basic + hra + da;
+
+    printf("Gross Salary = %.2f\n", salary);
+
+    return 0;
+}
